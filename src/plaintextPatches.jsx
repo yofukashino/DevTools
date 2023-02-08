11eeb56d76375b8086b365c307077abee4942a94
Tharki-god <alokender018@gmail.com>
2023-02-09 00:21:22 +0530

    update

--- src/plaintextPatches.jsx
+++ src/plaintextPatches.jsx
@@ -4,7 +4,7 @@ export default [
     replacements: [
       {
         match:
-          /([A-Za-z]+\([^)]*\)\([^)]*\)\.[0-9A-Za-z]+),([A-Za-z]+:\([^)]*\)\(_n\.Z,\{[A-Za-z]+:[A-Za-z]+,[A-Za-z]+:"friends-list")/gm,
+          /([A-Za-z]+\([^)]*\)\([^)]*\)\.[0-9A-Za-z]+),([A-Za-z]+:\([^)]*\)\([A-Za-z]+\.Z,\{[A-Za-z]+:[A-Za-z]+,[A-Za-z]+:"friends-list")/gm,
         replace: `$1,onContextMenu: (event) => HomeButtonContextMenuApi?.openContextMenu?.(event),$2`,
       },
     ],
