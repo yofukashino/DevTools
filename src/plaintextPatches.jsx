b83fcbbaf76fd9d23c4404f47e1b417337a5a821
Tharki-god <alokender018@gmail.com>
2023-02-14 02:44:47 +0530

    smaller and nicer regex

--- src/plaintextPatches.jsx
+++ src/plaintextPatches.jsx
@@ -3,9 +3,8 @@ export default [
     find: "Messages.DISCODO_DISABLED",
     replacements: [
       {
-        match:
-          /([A-Za-z]+\([^)]*\)\([^)]*\)\.[0-9A-Za-z]+),([A-Za-z]+:\([^)]*\)\([A-Za-z]+\.Z,\{[A-Za-z]+:[A-Za-z]+,[A-Za-z]+:"friends-list")/gm,
-        replace: `$1,onContextMenu: (event) => HomeButtonContextMenuApi?.openContextMenu?.(event),$2`,
+        match: /(className:.*\.[A-Za-z]+),(children.*"friends-list")/,
+        replace: `$1,onContextMenu: HomeButtonContextMenuApi?.openContextMenu,$2`,
       },
     ],
   },
